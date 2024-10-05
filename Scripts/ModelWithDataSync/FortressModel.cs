424349416bfc71b5717fdca698a27ed9b4b801d1
iffn <iffn1990@gmail.com>
2024-10-05 15:17:37 +0200

    Initial commit

---
+++ Scripts/ModelWithDataSync/FortressModel.cs
@@ -0,0 +1,73 @@
+﻿using System.Xml.Linq;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace iffnsStuff.iffnsVRCStuff.FortressBuilder
+{
+    public class FortressModel : UdonSharpBehaviour
+    {
+        [UdonSynced] int[] elementTypes = new int[0];
+        [UdonSynced] int[] xPos = new int[0];
+        [UdonSynced] int[] yPos = new int[0];
+        [UdonSynced] int[] zPos = new int[0];
+
+        public int[] ElementTypes { get { return elementTypes; } }
+        public int[] XPos { get { return xPos; } }
+        public int[] YPos { get { return yPos; } }
+        public int[] ZPos { get { return zPos; } }
+
+        int[] AddElementToArray(int[] baseArray, int number)
+        {
+            int[] returnArray = new int[baseArray.Length + 1];
+
+            for(int i = 0; i < baseArray.Length; i++)
+            {
+                returnArray[i] = baseArray[i];
+            }
+
+            returnArray[returnArray.Length - 1] = number;
+
+            return returnArray;
+        }
+
+        int[] RemoveElementFromArray(int[] baseArray, int indexToBeRemoved)
+        {
+            int[] returnArray = new int[baseArray.Length - 1];
+
+            int grabIndex = 0;
+
+            for (int i = 0; i < returnArray.Length; i++)
+            {
+                if (i == indexToBeRemoved) continue;
+
+                returnArray[i] = baseArray[grabIndex++];
+            }
+
+            return returnArray;
+        }
+
+        public void AddeElement(int element, int x, int y, int z)
+        {
+            elementTypes = AddElementToArray(elementTypes, element);
+            xPos = AddElementToArray(xPos, x);
+            yPos = AddElementToArray(yPos, y);
+            zPos = AddElementToArray(zPos, z);
+        }
+
+        public void RemoveElement(int index)
+        {
+            elementTypes = RemoveElementFromArray(elementTypes, index);
+            xPos = RemoveElementFromArray(xPos, index);
+            yPos = RemoveElementFromArray(yPos, index);
+            zPos = RemoveElementFromArray(zPos, index);
+        }
+
+        public void Sync()
+        {
+            if (!Networking.IsOwner(gameObject)) Networking.SetOwner(Networking.LocalPlayer, gameObject);
+            RequestSerialization();
+        }
+    }
+}
---
+++ Scripts/ViewToPlaceElements/FortressViewPlacingModels.cs
@@ -0,0 +1,34 @@
+﻿
+using iffnsStuff.iffnsVRCStuff.FortressBuilder;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class FortressViewPlacingModels : UdonSharpBehaviour
+{
+    [SerializeField] GameObject[] prefabElements;
+    [SerializeField] Transform elementHolder;
+    Vector3 gridSize = new Vector3(3f, 3f, 3f);
+
+    public void BuildAllElements(int[] elementTypes, int[] xPos, int[] yPos, int[] zPos)
+    {
+        for(int i = 0; i < elementTypes.Length; i++)
+        {
+            Transform newElement = GameObject.Instantiate(prefabElements[elementTypes[i]]).transform;
+            newElement.parent = elementHolder;
+            newElement.localPosition = new Vector3(
+                gridSize.x * xPos[i],
+                gridSize.y * yPos[i],
+                gridSize.z * zPos[i]);
+        }
+    }
+
+    public void RemoveAllElements()
+    {
+        foreach (Transform child in elementHolder)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+}
